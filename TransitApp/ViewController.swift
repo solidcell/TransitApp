bd4dc807ffccfd91b14b9cafe1e8b3cf445d801d
Jesse Farless <solidcell@gmail.com>
2016-10-25 15:08:06 +0200

    Initial commit

---
+++ TransitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitApp
+//
+//  Created by Jesse Farless on 10/25/16.
+//  Copyright © 2016 solidcell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
