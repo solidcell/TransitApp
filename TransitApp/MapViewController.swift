a972f74cef14738694ab8ec292bc95b649fe512c
Jesse Farless <solidcell@gmail.com>
2016-10-27 17:08:38 +0200

    Add a single hardcoded map annotation
    Now a provider is required and to actually parse
    the actual JSON if no data exists

--- TransitApp/MapViewController.swift
+++ TransitApp/MapViewController.swift
@@ -4,6 +4,28 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let initialLocation = CLLocation(latitude: 52.5302356, longitude: 13.4033659)
+        centerMapOnLocation(location: initialLocation)
+
+        let mapAnnotation = MapAnnotation(title: "title - Berlin",
+                                    locationName: "location name - Berlin",
+                                    discipline: "discipline - Berlin",
+                                    coordinate: CLLocationCoordinate2D(latitude: 52.5302356,
+                                                                       longitude: 13.4033659))
+        
+        mapView.addAnnotation(mapAnnotation)
+    }
+
+    private let regionRadius: CLLocationDistance = 10000
+    private func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
 }
 
