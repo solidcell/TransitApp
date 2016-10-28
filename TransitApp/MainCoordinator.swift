36dc065a5d4fecea4fd1848c6cf8cd1a562ac317
Jesse Farless <solidcell@gmail.com>
2016-10-28 09:55:59 +0200

    Use SeedDataParser in MainCoordinator

--- TransitApp/MainCoordinator.swift
+++ TransitApp/MainCoordinator.swift
@@ -1,10 +1,18 @@
 import UIKit
+import RealmSwift
 
 class MainCoordinator {
 
     private let mapCoordinator = MapCoordinator()
+    private let realm = try! Realm()
+    private let seedDataParser: SeedDataParser
+
+    init() {
+        seedDataParser = SeedDataParser(realm: realm)
+    }
 
     func start(window: UIWindow) {
+        seedDataParser.seedIfNeeded()
         mapCoordinator.start(window: window)
     }
     
