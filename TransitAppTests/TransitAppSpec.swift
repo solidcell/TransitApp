6cb13b797abb34717490ae72878a6381edfc8b77
Jesse Farless <solidcell@gmail.com>
2016-10-28 13:07:56 +0200

    Only create a new Realm in specs on demand

--- TransitAppTests/TransitAppSpec.swift
+++ TransitAppTests/TransitAppSpec.swift
@@ -4,12 +4,24 @@ import RealmSwift
 
 class TransitAppSpec: QuickSpec {
     
-    var realm: Realm!
+    var realm: Realm {
+        if let _realm = _realm { return _realm }
+
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = NSUUID().uuidString
+        let newRealm = try! Realm()
+        _realm = newRealm
+        return newRealm
+    }
+
+    private func newRealm() {
+        _realm = nil
+    }
+
+    private var _realm: Realm?
     
     override func spec() {
         beforeEach {
-            Realm.Configuration.defaultConfiguration.inMemoryIdentifier = NSUUID().uuidString
-            self.realm = try! Realm()
+            self.newRealm()
         }
     }
 }
